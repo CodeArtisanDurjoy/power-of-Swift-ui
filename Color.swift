9c43cbacc38ac6db259bef3750aa52b48554bf31
Durjoy Acharjya <da-durjoy@outlook.com>
2024-09-05 13:19:33 +0600

    Add files via upload
    Custom Hex Color Component

---
+++ Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  dwasa
+//
+//  Created by abdur.rahman on 9/1/24.
+//
+
+import SwiftUI
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
